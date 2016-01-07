a876cf6359b5aaebfb26a24b50d4e8627804392a
Karolina <karolina.zielniok@gmail.com>
2016-01-07 19:10:06 +0100

    initial commit

---
+++ src/main/java/com/zielniok/agot2/Main.java
@@ -0,0 +1,10 @@
+package com.zielniok.agot2;
+
+/**
+ * Created by Karolina on 2016-01-07.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
